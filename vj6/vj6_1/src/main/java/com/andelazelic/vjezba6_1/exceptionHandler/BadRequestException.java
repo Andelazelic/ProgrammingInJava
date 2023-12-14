9b92656a5e980a627c2cf4632f4536e63b6d40bf
Andelazelic <92513498+Andelazelic@users.noreply.github.com>
2023-12-14 11:25:58 +0100

    Delete vj6 directory

--- vj6/vj6_1/src/main/java/com/andelazelic/vjezba6_1/exceptionHandler/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.andelazelic.vjezba6_1.exceptionHandler;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- vj6/vj6_1/src/main/java/com/andelazelic/vjezba6_1/exceptionHandler/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.andelazelic.vjezba6_1.exceptionHandler;
-
-public class DataNotFoundException extends RuntimeException {
-    public DataNotFoundException(String message) {
-        super(message);
-    }
-}
